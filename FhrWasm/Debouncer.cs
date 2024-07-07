6f428b4fd6422a9ec4c8a294d6d7de1e94efb2c3
g l <glipitch@gmail.com>
2024-07-07 15:29:19 +0100

    Add Debouncer

---
+++ FhrWasm/Debouncer.cs
@@ -0,0 +1,18 @@
+﻿namespace FhrWasm;
+public class Debouncer<T>(Action<T> action, int delay = 200)
+{
+    CancellationTokenSource? source = null;
+    public void Debounce(T value)
+    {
+        source?.Cancel();
+        source = new CancellationTokenSource();
+        Task.Delay(delay, source.Token)
+            .ContinueWith(task =>
+            {
+                if (task.IsCompletedSuccessfully)
+                {
+                    action(value);
+                }
+            }, TaskScheduler.Default);
+    }
+}
